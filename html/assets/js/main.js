1548cbcb7d8250cc3bd13bd315588054f6f09c48
bitsdanielsalinas <37351796+bitsdanielsalinas@users.noreply.github.com>
2018-06-21 15:05:00 -0500

    footer

---
+++ html/assets/js/main.js
@@ -0,0 +1,19 @@
+/**
+ * @file
+ * Custom scripts for theme.
+ */
+(function ($) {
+
+  // Hello World.
+  Drupal.behaviors.helloWorld = {
+    attach: function (context) {
+      console.log('Hello Wordld');
+    }
+  }
+
+  // Hello World.
+  Drupal.behaviors.menuMain = {
+     
+  }
+
+})(jQuery);
