36e7c2d3cba40971dfa2c273e8f81fb818526399
Johan Chaparro <Johasanchez@misena.edu.co>
2018-08-29 17:04:51 -0500

    Retoques de estilos y html  de carousel-controls

--- angular/src/app/cards/components/carousel-controls/carousel-controls.component.ts
+++ angular/src/app/cards/components/carousel-controls/carousel-controls.component.ts
@@ -16,28 +16,11 @@ export class CarouselControlsComponent implements OnInit {
   CarouselControlArray: any = [];
     ngOnInit() {
         // this.getCarouselControlItem();
-      $('.carousel-run .carousel-item').each(function () {
-            // tslint:disable-next-line:no-var-keyword
-            var next = $(this).next();
-          if (!next.length) {
-              next = $(this).siblings(':first');
-          }
-          next.children(':first-child').clone().appendTo($(this));
-
-          // tslint:disable-next-line:no-var-keyword
-          for (var i = 0; i < 2; i++) {
-              next = next.next();
-              if (!next.length) {
-                  next = $(this).siblings(':first');
-              }
-              next.children(':first-child').clone().appendTo($(this));
-            }
-        });
         this.getCarouselControlItem();
     }
          getCarouselControlItem() {
           return this.CarouselControl.getCarouselControlItems().subscribe(items => {
-            //console.log(items);
+            // console.log(items);
             this.CarouselControlArray = items;
            // console.log(this.CarouselControlArray);
           });
