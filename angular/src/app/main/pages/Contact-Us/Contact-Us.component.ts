d370cef3f8e5dded417417afb7d8f40a0e4fa46e
Andres Figueroa <andres.figueroa@bitsamericas.com>
2018-09-13 09:29:23 -0500

    banner component

---
+++ angular/src/app/main/pages/Contact-Us/Contact-Us.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-contact-us',
+    templateUrl: './Contact-Us.component.html',
+    styleUrls: ['./Contact-Us.component.scss']
+})
+export class ContactUsComponent {
+ constructor() {}
+}
