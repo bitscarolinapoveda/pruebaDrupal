fdd7bc2622fb8f63055bc2fdb3a66779e2429d9e
bitscristianhenao <cristian.henao@bitsamericas.com>
2018-10-09 16:03:31 -0500

    Solucion de conflictos

--- angular/src/app/services/cards/v1-card.services.ts
+++ angular/src/app/services/cards/v1-card.services.ts
@@ -17,6 +17,7 @@ export class CustomCardService {
     let url = `v1/card/config/${idblock}/export?_format=json`;
     return this.http.get(url);
   }
+
   getFooterContactItems() {
     return this.http.get('v1/card/contact/export/custom?_format=json&bid=contactcard');
   }
