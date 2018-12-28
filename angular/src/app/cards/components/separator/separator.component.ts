49ea99ba27934305fe74c605ae17e43b3b43572a
BitsJohnLeon <john.leon@bitsamericas.com>
2018-12-28 11:31:22 -0500

    Ajustando SeparatorComponent
    Se ajusta que el icono del separador reciba como parametro desde el padre, el nombre del icono a utilizar
    
    Este icono se contruye en base a la FontAwesome Tool

--- angular/src/app/cards/components/separator/separator.component.ts
+++ angular/src/app/cards/components/separator/separator.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'app-separator',
@@ -6,6 +6,9 @@ import { Component } from '@angular/core';
     styleUrls: ['./separator.component.scss']
 })
 export class SeparatorComponent {
+
+    @Input() iconCustom:string;
+
     constructor(){
         
     }
