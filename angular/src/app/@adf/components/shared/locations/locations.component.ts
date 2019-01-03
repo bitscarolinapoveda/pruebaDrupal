7f847102ba35a3da9dee6fe1a1516ce336343655
bitscristianhenao <cristian.henao@bitsamericas.com>
2019-01-03 09:09:09 -0500

    SITIOBITS | FRONT | Se ajustan los estilos de location

--- angular/src/app/@adf/components/shared/locations/locations.component.ts
+++ angular/src/app/@adf/components/shared/locations/locations.component.ts
@@ -9,10 +9,18 @@ import {HttpService} from '../../../../services/http/http.service';
 export class LocationsComponent implements OnInit {
   public imageIcon = '/assets/icon/iconBitsLocationMarker.svg';
   public closeOthers = true;
+  public infoWindowOpen = true;
   public locations_data: any;
   public lat: number;
   public lng: number;
+  public titleCity;
+  public addressCity;
+  public colorWhileLoad = 'rgb(202, 202, 202)';
+  public selected;
   public map: any;
+  public mobileScreen = false;
+  public desktopScreen = false;
+  public widthOftheMap;
   public defaultZoomMap = 15;
   public bitsTypeMap: any = [
     {
@@ -47,35 +55,51 @@ export class LocationsComponent implements OnInit {
     private _http: HttpService,
   ) {
     this.locations_data = [];
-    //Coordenadas de Bogotá
-    this.lat = 4.6097102;
-    this.lng = -74.081749;
+    this.infoWindowOpen = true;
   }
-
   ngOnInit () {
-    //Se conecta direcamente con el módulo adf_location en Drupal
     this._http.get('/location-resources').subscribe((items) => {
       for (let index = 0; index < items.length; index++) {
         items[index].lat = parseFloat(items[index].lat);
         items[index].lng = parseFloat(items[index].lng);
       }
+      this.lat = items[0].lat;
+      this.lng = items[0].lng;
+      this.titleCity = items[0].title;
+      this.addressCity = items[0].address;
       this.locations_data = items;
     });
+    this.onResize();
   }
-
-  // mapReady (map) {
-  //   this.map = map;
-  // }
-
-  /***
-   * Al hacer click en una ubicación, la "centra" en el mapa
-   * @param lat
-   * @param lng
-   */
-  setCenter (lat, lng) {
-    let pos = {lat: parseFloat(lat), lng: parseFloat(lng)};
-    this.map.setCenter(pos);
-    this.map.setZoom(15);
+  onResize() {
+    const widthScreen = (window.innerWidth);
+    if (widthScreen < 1164) {
+      this.mobileScreen = true;
+      this.desktopScreen = false;
+    } else {
+      this.mobileScreen = false;
+      this.desktopScreen = true;
+    }
+    this.sizeOfMap();
+  }
+  sizeOfMap() {
+    if (document.readyState === 'complete') {
+      const widthOfDiv = document.querySelector('.map-location-desktop').clientWidth + 'px';
+      this.widthOftheMap = widthOfDiv;
+    } else {
+    return false;
+    }
+  }
+  ubicateCity (ubicacion, index) {
+    this.lat = parseFloat(ubicacion.lat);
+    this.lng = parseFloat(ubicacion.lng);
+    this.titleCity = ubicacion.title;
+    this.addressCity = ubicacion.address;
+    this.selected = 'selected';
+    console.log(index);
+  }
+  scrollMaps() {
+    document.querySelector('.row.location-button').scrollIntoView();
   }
 }
 
