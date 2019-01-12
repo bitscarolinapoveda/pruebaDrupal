f3bc8e926b3eeaad31aea33deb8f79c915bdb9ee
Julian David Guerrero Pinilla <julian.guerrero@bitsamericas.com>
2019-01-12 09:16:12 -0500

    Menu | Permite cargar imagenes den el menu

--- angular/src/app/layout/components/navbar/navbar.component.ts
+++ angular/src/app/layout/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewEncapsulation, HostListener} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {NavbarService} from '../../../services/layout/navbar.service';
 
@@ -7,13 +7,17 @@ declare var $: any;
 @Component({
     selector: 'app-navbar',
     templateUrl: './navbar.component.html',
-    styleUrls: ['./navbar.component.scss']
+    styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
-    constructor(private router: ActivatedRoute, private navbar: NavbarService) {
-    }
+    NavbarArray: any;
+    positionChild: number = 1;
+    resolution: string = 'desktop';
+    flActiveChilds: any[] = [];
 
-    NavbarArray: any = [];
+    constructor(private router: ActivatedRoute, private navbar: NavbarService) {
+        this.onResize({});
+    }
 
     ngOnInit() {
         $('.Botton, .show-more').on('click', function () {
@@ -32,10 +36,53 @@ export class NavbarComponent implements OnInit {
     getNavBarItems() {
         return this.navbar.getMenuItems().subscribe(items => {
             this.NavbarArray = items;
+            this.NavbarArray = this.addIdForColToNavbar(this.NavbarArray, 1, 1);
+            console.log(this.NavbarArray);
         });
     }
 
-    activeClass(sub) {
-        sub.active = !sub.active;
+    addIdForColToNavbar(navbarArray, posCol, posRow) {
+
+        for (let i in navbarArray) {
+            navbarArray[i].posCol=posCol;
+            navbarArray[i].posRow=posRow;
+
+            if (posRow == 1) {
+                navbarArray[i].showrow = true;
+            }
+            else {
+                navbarArray[i].showrow = false;
+            }
+
+            if (navbarArray[i].below) {
+                navbarArray.below = this.addIdForColToNavbar(navbarArray[i].below, i, posRow + 1);
+            }
+        }
+        return navbarArray;
+    }
+
+    activeChild(idx) {
+        if (!this.flActiveChilds[idx]) {
+            this.flActiveChilds[idx] = false;
+        }
+        this.flActiveChilds[idx] = !this.flActiveChilds[idx];
+        //this.addchild.emit(info);
+        //return this.navBar.activeChild(event, col, row);
+    }
+    getActiveChild(idx) {
+        if (!this.flActiveChilds[idx]) {
+            this.flActiveChilds[idx] = false;
+        }
+        return this.flActiveChilds[idx];
+    }
+
+    @HostListener('window:resize', ['$event'])
+    onResize(event) {
+      if ($('body').width() < 1025) {
+        this.resolution = 'mobile';
+      }
+      else {
+        this.resolution = 'desktop';
+      }
     }
 }
