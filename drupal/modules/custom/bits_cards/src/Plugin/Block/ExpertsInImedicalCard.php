b3f4e8dac58fc0954d25b52a7d089630a9bdf700
Antonio Cardona E <antonio.cardona@bitsamericas.com>
2019-02-15 10:59:53 -0500

    + Se agrega el Tipo de contenido imedical_expertsin. + Se crea card ExpertsInImedicalCard.

---
+++ drupal/modules/custom/bits_cards/src/Plugin/Block/ExpertsInImedicalCard.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Drupal\bits_cards\Plugin\Block;
+
+use Drupal\adf_cards\Plugin\Block\CardBase;
+
+/**
+ * Provides a 'ExpertsInImedicalCard' block.
+ *
+ * @Block(
+ *  id = "experts_in_imedical_card",
+ *  admin_label = @Translation("Experts in imedical card"),
+ * )
+ */
+class ExpertsInImedicalCard extends CardBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return [
+      'header' => [
+        'table_fields' => [
+          'title' => [
+            'type' => 'textfield',
+            'title' => $this->t('Título'),
+            'service_field' => 'title',
+            'show' => 1,
+            'weight' => 1,
+            'max_length' => 50,
+          ],
+        ],
+      ],
+      'body' => [],
+      'entity' => [
+        'name' => 'node',
+        'type' => 'imedical_expertsin',
+        'limit' => 5,
+        'offset' => 0,
+      ],
+      'others' => [],
+    ];
+  }
+
+}
