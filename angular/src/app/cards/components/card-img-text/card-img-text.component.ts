a6194a3977b8acb2827839dd08091582767d9c5f
unknown <alegna.Martinez@bitsamericas.com>
2019-02-20 13:34:05 -0500

    Consumir servicio en componente app-card-img-text

--- angular/src/app/cards/components/card-img-text/card-img-text.component.ts
+++ angular/src/app/cards/components/card-img-text/card-img-text.component.ts
@@ -1,47 +1,64 @@
-import { OnInit, Component, Input } from "@angular/core";
-import { CustomCardService } from "src/app/services/cards/v1-card.services";
+import { OnInit, Component, Input } from '@angular/core';
+import { CustomCardService } from 'src/app/services/cards/v1-card.services';
 
 @Component({
     selector: 'app-card-img-text',
     templateUrl: './card-img-text.component.html',
     styleUrls: ['card-img-text.component.scss']
 })
-export class CardImgTextComponent implements OnInit{
-    
-    tituloCard:string;
+export class CardImgTextComponent implements OnInit {
+    public titleL: string;
+    public cardImgL: any[];
+    public titleR: string;
+    public cardImgR: any[];
+
+    /* tituloCard:string;
     subitituloCard:string;
     descripcionCard:string;
     linkCard:string;
     linkLabelCard:string;
-    imagenCard:string;
-    //Si se recibe IT : Imagen luego texto
-    //Si se recibe TI : Texto luego imagen
-    @Input() orientacion:string;
-    
+     */
+    // imagenCard: string;
+    // Si se recibe IT : Imagen luego texto
+    // Si se recibe TI : Texto luego imagen
+    @Input() orientacion: string;
+
     constructor(
-        private _http:CustomCardService
+        private _http: CustomCardService
     ) {
-        this.tituloCard = "Título de este card con imagen";
-        this.subitituloCard = "Sint labore exercitation sint voluptate aliqua commodo.";
-        this.descripcionCard = "Lorem consectetur in enim cillum duis duis excepteur et minim adipisicing. Sint ea irure laboris in do irure do duis irure sint. Esse irure ex irure mollit exercitation. Nulla adipisicing enim reprehenderit occaecat ullamco eiusmod duis cupidatat quis. Elit aliquip officia deserunt tempor minim officia aliquip mollit minim anim nostrud veniam deserunt culpa.";
+        /* this.tituloCard = "Título de este card con imagen";
+        this.subitituloCard = "Sint labore exercitation sint voluptate aliqua commodo Alegna.";
+        this.descripcionCard = "Lorem ";
         this.linkCard = "www.loremipsum.com";
         this.linkLabelCard = "Ver CASOS DE ÉXITO de este producto";
-        this.imagenCard = "assets/images/imagen-iz-de-web.png";
-        this.orientacion = "";
+         */
+        // this.imagenCard = 'assets/images/imagen-iz-de-web.png';
+        this.orientacion = '';
+        this.cardImgL = [];
+        this.cardImgR = [];
     }
 
-    ngOnInit(){
-        //this.getCardImgTextService();
+    ngOnInit() {
+        this.getCardImgTextServiceL();
+        this.getCardImgTextServiceR();
     }
 
-    getCardImgTextService(){
-        this._http.getCustomCardInformation('card-img-text').subscribe( params => {
-            this.tituloCard = params.data[0].titulo;
+    getCardImgTextServiceL() {
+        this._http.getCustomCardInformation('mediaimedicalcard_2').subscribe(params => {
+            /* this.tituloCard = params.data[0].titulo;
             this.subitituloCard = params.data[0].subtitulo;
             this.descripcionCard = params.data[0].descripcion;
             this.linkCard = params.data[0].link;
             this.linkLabelCard = params.data[0].label;
-            this.imagenCard = params.data[0].imagen;
+            this.imagenCard = params.data[0].imagen;  */
+            this.titleL = params.header[0].data.title;
+            this.cardImgL = params.data;
+        });
+    }
+    getCardImgTextServiceR() {
+        this._http.getCustomCardInformation('mediaimedicalcard').subscribe(params => {
+            this.titleR = params.header[0].data.title;
+            this.cardImgR = params.data;
         });
     }
 }
