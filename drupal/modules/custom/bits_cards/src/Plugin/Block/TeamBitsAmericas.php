7c453b0f547390cb064ca27818f65b37420afc0f
Omar Guevara Riveros <omar.guevara@bitsamericas.com>
2019-02-25 08:02:07 -0500

    Back | Cards Achie, Know, Team, Testi y Why
    Cards, Achievements, Know, Team, Testimonies, Why

---
+++ drupal/modules/custom/bits_cards/src/Plugin/Block/TeamBitsAmericas.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Drupal\bits_cards\Plugin\Block;
+
+use Drupal\adf_cards\Plugin\Block\CardBase;
+
+/**
+ * Provides a 'TeamBitsAmericas' block.
+ *
+ * @Block(
+ *  id = "team_bits_americas",
+ *  admin_label = @Translation("Team Bits Americas"),
+ * )
+ */
+class TeamBitsAmericas extends CardBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return [
+      'header' => [
+        'table_fields' => [
+          'title' => [
+            'type' => 'textfield',
+            'title' => $this->t('Título'),
+            'service_field' => 'title',
+            'show' => 1,
+            'weight' => 1,
+            'max_length' => 50,
+          ],
+          'sub_title' => [
+            'type' => 'textfield',
+            'title' => $this->t('Sub - Título'),
+            'service_field' => 'sub_title',
+            'show' => 1,
+            'weight' => 1,
+            'max_length' => 50,
+          ],
+        ],
+      ],
+      'body' => [],
+      'files' => [],
+      'entity' => [
+        'name' => 'node',
+        'type' => 'equipo_de_trabajo',
+        'limit' => 4,
+        'offset' => 0,
+        'default_view_mode' => 'default',
+      ],
+      'others' => [],
+    ];
+  }
+
+}
