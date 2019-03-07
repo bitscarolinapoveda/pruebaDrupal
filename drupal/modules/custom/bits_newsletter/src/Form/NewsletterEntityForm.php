cd91fa591d87cb8ba7c9ebdb47368052222a5d72
Daniel Arturo Casals Amat <dcasalsamat@gmail.com>
2019-03-07 16:36:43 -0500

    Subiendo Entidad de contenido Newsletter

---
+++ drupal/modules/custom/bits_newsletter/src/Form/NewsletterEntityForm.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Drupal\bits_newsletter\Form;
+
+use Drupal\Core\Entity\ContentEntityForm;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Form controller for Newsletter entity edit forms.
+ *
+ * @ingroup bits_newsletter
+ */
+class NewsletterEntityForm extends ContentEntityForm {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    /* @var $entity \Drupal\bits_newsletter\Entity\NewsletterEntity */
+    $form = parent::buildForm($form, $form_state);
+
+    $entity = $this->entity;
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function save(array $form, FormStateInterface $form_state) {
+    $entity = $this->entity;
+
+    $status = parent::save($form, $form_state);
+
+    switch ($status) {
+      case SAVED_NEW:
+        drupal_set_message($this->t('Created the %label Newsletter entity.', [
+          '%label' => $entity->label(),
+        ]));
+        break;
+
+      default:
+        drupal_set_message($this->t('Saved the %label Newsletter entity.', [
+          '%label' => $entity->label(),
+        ]));
+    }
+    $form_state->setRedirect('entity.newsletter_entity.canonical', ['newsletter_entity' => $entity->id()]);
+  }
+
+}
---
+++ drupal/modules/custom/bits_newsletter/src/Form/NewsletterEntitySettingsForm.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Drupal\bits_newsletter\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Class NewsletterEntitySettingsForm.
+ *
+ * @ingroup bits_newsletter
+ */
+class NewsletterEntitySettingsForm extends FormBase {
+
+  /**
+   * Returns a unique string identifying the form.
+   *
+   * @return string
+   *   The unique string identifying the form.
+   */
+  public function getFormId() {
+    return 'newsletterentity_settings';
+  }
+
+  /**
+   * Form submission handler.
+   *
+   * @param array $form
+   *   An associative array containing the structure of the form.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The current state of the form.
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    // Empty implementation of the abstract submit class.
+  }
+
+  /**
+   * Defines the settings form for Newsletter entity entities.
+   *
+   * @param array $form
+   *   An associative array containing the structure of the form.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The current state of the form.
+   *
+   * @return array
+   *   Form definition array.
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form['newsletterentity_settings']['#markup'] = 'Settings form for Newsletter entity entities. Manage field settings here.';
+    return $form;
+  }
+
+}
