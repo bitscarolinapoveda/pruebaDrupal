d307bcd2ba40eab7fcfad70dbefd28f1639dcefa
unknown <alegna.Martinez@bitsamericas.com>
2019-03-27 12:50:51 -0400

    Creación de nuevo componente app-screenshots.

---
+++ angular/src/app/cards/components/screenshots/screenshots.component.ts
@@ -0,0 +1,76 @@
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import { CustomCardService } from './../../../services/cards/v1-card.services';
+import { DataMenu } from '../menu-template/menu-template.component';
+import { NgxCarousel } from 'ngx-carousel';
+
+@Component({
+  selector: 'app-screenshots',
+  templateUrl: './screenshots.component.html',
+  styleUrls: ['./screenshots.component.scss']
+})
+export class ScreenshotsComponent implements OnInit {
+
+  public screenshotsTitle;
+  public screenshotsSubTitle;
+  public screenshotsData;
+  visible: boolean;
+
+  @Output() propagar = new EventEmitter<DataMenu>();
+  datosMenu: DataMenu;
+  @Input() type: string;
+
+  CarouselControlArray: any = [];
+  public carouselOne: NgxCarousel;
+
+  constructor(private screenshots: CustomCardService) { }
+
+  ngOnInit() {
+    this.datosMenu = {
+      label: 'SCRENSHOTS',
+      id: 'a18',
+      url: '/imedical'
+    };
+    this.propagar.emit(this.datosMenu);
+    this.getScreenshotsInfo();
+    this.carouselOne = {
+      grid: { xs: 3, sm: 3, md: 4, lg: 4, all: 0 },
+      slide: 2,
+      speed: 400,
+      animation: 'lazy',
+      point: {
+        visible: true
+      },
+      load: 4,
+      touch: true,
+      easing: 'ease',
+      loop: true,
+    };
+  }
+
+  getScreenshotsInfo() {
+    return this.screenshots.getCustomCardInformation('screenshotscard').subscribe(items => {
+      var list = [];
+      this.screenshotsTitle = items.header[0].data.title;
+      this.screenshotsSubTitle = items.header[1].data.sub_title;
+      this.CarouselControlArray = items.data;
+      for (let index = 0; index < items.data.length; index++) {
+        for (let j = 0; j < items.data[index].field_image.length; j++) {
+          list.push(items.data[index].field_image[j]);
+          list.push(items.data[index].field_image[j]);
+        }
+      }
+      this.CarouselControlArray = list;
+      if (this.screenshotsTitle !== '' && this.CarouselControlArray.length !== 0) {
+        this.visible = true;
+      }
+    });
+  }
+
+  public carouselTileLoad(evt: any) {
+    const len = this.CarouselControlArray.length;
+    if (len <= 4) {
+      for (let i = len; i < len + 10; i++) { this.CarouselControlArray.push(i); }
+    }
+  }
+
+}
