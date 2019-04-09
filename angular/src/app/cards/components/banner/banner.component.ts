4cb900dca10289df973caa4719177d37af62efc9
unknown <alegna.Martinez@bitsamericas.com>
2019-04-09 17:25:10 -0400

    Agregar consumo del nuevo servicio “allproductsandservicescard_2”  componente: app-banner.

--- angular/src/app/cards/components/banner/banner.component.ts
+++ angular/src/app/cards/components/banner/banner.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CustomCardService } from 'src/app/services/cards/v1-card.services';
-
+import { General } from '../blurb/blurb.component';
 @Component({
     selector: 'app-banner',
     templateUrl: './banner.component.html',
@@ -15,6 +15,8 @@ export class BannerComponent implements OnInit {
     bannerBackground: any = [];
     medius = '550px';
     bannerDescrip: string;
+    principalBanner: General;
+
     constructor(
         router: ActivatedRoute,
         private banner: CustomCardService,
@@ -25,17 +27,22 @@ export class BannerComponent implements OnInit {
     }
 
     ngOnInit() {
+        if (this.titulo !== '' && (this.imgFondo === '' || this.imgFondo === undefined)) {
+            this.getProductsAndServicesItems();
+        } else if (this.uuid === undefined || this.uuid === '') {
+            this.getTitle();
+            this.bannerTitle = this.titulo;
+        } else {
+            this.getTitle();
+            this.getBannerService();
+        }
+    }
+
+    getTitle() {
         if (this.titulo !== '') {
             while (this.titulo.indexOf('-') > -1) {
                 this.titulo = this.titulo.replace('-', '_');
             }
-        }
-        if (this.titulo !== '' && (this.imgFondo === '' || this.imgFondo === undefined)) {
-            this.getProductsAndServicesItems();
-        } else if (this.uuid === undefined || this.uuid === '') {
-            this.bannerTitle = this.titulo;
-        } else {
-            this.getBannerService();
         }
     }
 
@@ -55,16 +62,19 @@ export class BannerComponent implements OnInit {
     }
 
     getProductsAndServicesItems() {
-        this.banner.getCustomCardInformation('productsandservicescard_2').subscribe(items => {
-            const servicesProduct = items.data;
+        this.banner.getCustomCardInformation('allproductsandservicescard_2').subscribe(items => {
+            this.principalBanner = this.banner.clone(items);
+            this.principalBanner.data = this.banner.addImageField(this.principalBanner.data, ['field_short_image']);
+            this.principalBanner.data = this.banner.addImageField(this.principalBanner.data, ['field_large_image']);
+            const servicesProduct = this.principalBanner.data;
             for (let index = 0; index < servicesProduct.length; index++) {
-                if (servicesProduct[index].nid === this.titulo) {
-                    this.bannerBackground = servicesProduct[index].large_image[0].url;
+                if (servicesProduct[index].url.indexOf(this.titulo) > -1 && servicesProduct[index].field_large_image !== undefined) {
+                    this.bannerBackground = servicesProduct[index].field_large_image.url;
                 }
             }
             if (this.titulo !== '') {
-                while (this.titulo.indexOf('_') > -1) {
-                    this.titulo = this.titulo.replace('_', ' ');
+                while (this.titulo.indexOf('-') > -1) {
+                    this.titulo = this.titulo.replace('-', ' ');
                 }
             }
             this.bannerTitle = this.titulo;
