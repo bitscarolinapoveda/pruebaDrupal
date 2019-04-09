f1fc1a801561dd1d4b70c7c590b60d16c889db1e
unknown <alegna.Martinez@bitsamericas.com>
2019-04-09 17:51:15 -0400

    Agregar consumo del nuevo servicio “allproductsandservicescard_2”  en componente app-testimonies. Validaciones.

--- angular/src/app/cards/components/testimonies/testimonies.component.ts
+++ angular/src/app/cards/components/testimonies/testimonies.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { CustomCardService } from './../../../services/cards/v1-card.services';
 import { DataMenu } from '../menu-template/menu-template.component';
+import { General } from '../blurb/blurb.component';
 
 @Component({
   selector: 'app-testimonies',
@@ -13,6 +14,8 @@ export class TestimoniesComponent implements OnInit {
   public testimoniesSubTitle;
   public testimoniesData;
   visible: boolean;
+  principalTestimonies: General;
+  @Input() type: string;
 
   @Output() propagar = new EventEmitter<DataMenu>();
   datosMenu: DataMenu;
@@ -26,22 +29,46 @@ export class TestimoniesComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.datosMenu = {
-      label: 'TESTIMONIES',
-      id: 'a12',
-      url: '/imedical'
-    };
-    this.propagar.emit(this.datosMenu);
     this.getTestimoniesInfo();
   }
   getTestimoniesInfo() {
-    return this.testimonies.getCustomCardInformation('testimoniesbitsamericas').subscribe(items => {
+    /* return this.testimonies.getCustomCardInformation('testimoniesbitsamericas').subscribe(items => {
       this.testimoniesTitle = items.header[0].data.title;
       this.testimoniesSubTitle = items.header[1].data.sub_title;
       this.testimoniesData = items.data;
       if (this.testimoniesTitle !== '' && this.testimoniesData.length !== 0) {
         this.visible = true;
+        this.datosMenu = {
+          label: 'TESTIMONIES',
+          id: 'a12',
+          url: '/imedical'
+        };
+        this.propagar.emit(this.datosMenu);
       }
+    }); */
+
+    this.testimonies.getCustomCardInformation('allproductsandservicescard_2').subscribe(items => {
+      this.principalTestimonies = this.testimonies.getFilterPrincipalType(items, 'field_testimonies', this.type);
+
+      this.testimonies.getCustomCardInformation('testimoniesbitsamericas').subscribe(itemsw => {
+        this.testimoniesTitle = itemsw.header[0].data.title;
+        this.testimoniesSubTitle = itemsw.header[1].data.sub_title;
+
+        const list_items = itemsw.data;
+
+        this.testimoniesData = this.testimonies.getFilterLists(this.principalTestimonies, itemsw.data);
+
+        if (this.testimoniesTitle !== '' && this.testimoniesData.length !== 0) {
+          this.visible = true;
+          this.datosMenu = {
+            label: 'TESTIMONIES',
+            id: 'a12',
+            url: '/imedical'
+          };
+          this.propagar.emit(this.datosMenu);
+        }
+
+      });
     });
   }
 }
