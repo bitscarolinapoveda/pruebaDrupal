041552353e329d72578f22932a3b963332617837
unknown <alegna.Martinez@bitsamericas.com>
2019-04-10 13:37:39 -0400

    Agregar consumo del nuevo servicio “allproductsandservicescard_2” en componente app-video-information. Validaciones.

--- angular/src/app/cards/components/video-information/video-information.component.ts
+++ angular/src/app/cards/components/video-information/video-information.component.ts
@@ -2,27 +2,23 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { CustomCardService } from 'src/app/services/cards/v1-card.services';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import { DataMenu } from '../menu-template/menu-template.component';
-
+import { General } from '../blurb/blurb.component';
 @Component({
     selector: 'app-video-information',
     templateUrl: './video-information.component.html',
     styleUrls: ['./video-information.component.scss']
 })
 export class VideoInformationComponent implements OnInit {
-
     videoURL: string;
     videoURLSanitizer: SafeUrl;
     titleSection: string;
     descriptionSection: string;
     link: string;
-
     @Output() propagar = new EventEmitter<DataMenu>();
-
     datosMenu: DataMenu;
-
     @Input() type: string;
-
     visible: boolean;
+    principalVideoInformation: General;
 
     constructor(
         private https: CustomCardService,
@@ -31,38 +27,39 @@ export class VideoInformationComponent implements OnInit {
     }
 
     ngOnInit() {
-        while (this.type.indexOf('-') > -1) {
-            this.type = this.type.replace('-', '_');
-        }
-
-        this.datosMenu = {
-            label: 'VIDEOS',
-            id: 'a7',
-            url: '/imedical'
-        };
-
-        this.propagar.emit(this.datosMenu);
         this.getVideoInformationService();
     }
 
     getVideoInformationService() {
-        this.https.getCustomCardInformationType('videoplusinformation', this.type).subscribe(items => {
-            this.titleSection = items.data[0].title;
-            this.descriptionSection = items.data[0].body;
-            this.videoURL = items.data[0].field_url_video[0].uri;
-            if (this.videoURL.includes('watch?v=')) {
-                this.videoURLSanitizer = this.sanitizer
-                    .bypassSecurityTrustResourceUrl(`${this.videoURL.split('watch?v=')[0]}embed/${this.videoURL.split('watch?v=')[1]}`);
-            } else {
-                this.videoURLSanitizer = this.sanitizer.bypassSecurityTrustResourceUrl(this.videoURL);
-            }
-            this.link = items.data[0].field_url_video[0].uri;
+        this.https.getCustomCardInformation('allproductsandservicescard_2').subscribe(items => {
+            this.principalVideoInformation = this.https.getFilterPrincipalType(items, 'field_video', this.type);
 
-            if (this.titleSection !== '' && this.videoURLSanitizer !== '') {
-                this.visible = true;
+            this.https.getCustomCardInformation('videoplusinformation').subscribe(params => {
+                params = this.https.getFilterLists(this.principalVideoInformation, params);
+                if (params.data.length !== 0) {
+                    this.titleSection = params.data[0].title;
+                    this.descriptionSection = params.data[0].body;
+                    this.videoURL = params.data[0].field_url_video[0].uri;
+                    if (this.videoURL.includes('watch?v=')) {
+                        this.videoURLSanitizer = this.sanitizer
+                            .bypassSecurityTrustResourceUrl(`${this.videoURL.split('watch?v=')
+                            [0]}embed/${this.videoURL.split('watch?v=')[1]}`);
+                    } else {
+                        this.videoURLSanitizer = this.sanitizer.bypassSecurityTrustResourceUrl(this.videoURL);
+                    }
+                    this.link = params.data[0].field_url_video[0].uri;
+                    if (this.titleSection !== '' && this.videoURLSanitizer !== '') {
+                        this.visible = true;
+                        this.datosMenu = {
+                            label: 'VIDEOS',
+                            id: 'a7',
+                            url: '/imedical'
+                        };
+                        this.propagar.emit(this.datosMenu);
+                    }
+                }
             }
-        }
-        );
-
+            );
+        });
     }
 }
