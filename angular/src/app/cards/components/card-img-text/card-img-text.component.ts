5dc3f23e5b73564afc235e59d94242cf9b0f5293
unknown <alegna.Martinez@bitsamericas.com>
2019-04-10 13:33:46 -0400

    Agregar consumo del nuevo servicio “allproductsandservicescard_2” en componente app-card-img-text. Validaciones.

--- angular/src/app/cards/components/card-img-text/card-img-text.component.ts
+++ angular/src/app/cards/components/card-img-text/card-img-text.component.ts
@@ -1,7 +1,7 @@
 import { OnInit, Component, Input, Output, EventEmitter } from '@angular/core';
 import { CustomCardService } from 'src/app/services/cards/v1-card.services';
 import { DataMenu } from '../menu-template/menu-template.component';
-
+import { General } from '../blurb/blurb.component';
 @Component({
     selector: 'app-card-img-text',
     templateUrl: './card-img-text.component.html',
@@ -14,15 +14,13 @@ export class CardImgTextComponent implements OnInit {
     public cardImgR: any[];
     visibleL: boolean;
     visibleR: boolean;
-
+    principalL: General;
+    principalR: General;
     // Si se recibe IT : Imagen luego texto
     // Si se recibe TI : Texto luego imagen
     @Input() orientacion: string;
-
     @Output() propagar = new EventEmitter<DataMenu>();
-
     datosMenu: DataMenu;
-
     @Input() type: string;
 
     constructor(
@@ -36,49 +34,52 @@ export class CardImgTextComponent implements OnInit {
     }
 
     ngOnInit() {
-        while (this.type.indexOf('-') > -1) {
-            this.type = this.type.replace('-', '_');
-        }
-
-        if (this.orientacion === 'IT') {
-            this.datosMenu = {
-                label: 'IMAG L',
-                id: 'a3',
-                url: '/imedical'
-            };
-        } else {
-            this.datosMenu = {
-                label: 'IMAG R',
-                id: 'a4',
-                url: '/imedical'
-            };
-        }
-
-        this.propagar.emit(this.datosMenu);
-
         this.getCardImgTextServiceL();
         this.getCardImgTextServiceR();
     }
 
     getCardImgTextServiceL() {
-        this._http.getCustomCardInformationType('mediaimedicalcard_2', this.type).subscribe(params => {
-            this.titleL = params.header[0].data.title;
-            params.data = this._http.addImageField(params.data, ['field_imagen_media_product']);
-            this.cardImgL = params.data;
-            if (this.cardImgL.length !== 0) {
-                this.visibleL = true;
-            }
+        this._http.getCustomCardInformation('allproductsandservicescard_2').subscribe(items => {
+            this.principalL = this._http.getFilterPrincipalType(items, 'field_left_media', this.type);
+
+            this._http.getCustomCardInformation('mediaimedicalcard_2').subscribe(params => {
+                params = this._http.getFilterLists(this.principalL, params);
+                this.titleL = params.header[0].data.title;
+                params.data = this._http.addImageField(params.data, ['field_imagen_media_product']);
+                this.cardImgL = params.data;
+                if (this.cardImgL.length !== 0) {
+                    this.visibleL = true;
+                    this.datosMenu = {
+                        label: 'IMAG L',
+                        id: 'a3',
+                        url: '/imedical'
+                    };
+                    this.propagar.emit(this.datosMenu);
+                }
+            });
         });
 
     }
     getCardImgTextServiceR() {
-        this._http.getCustomCardInformationType('mediaimedicalcard', this.type).subscribe(params => {
-            this.titleR = params.header[0].data.title;
-            params.data = this._http.addImageField(params.data, ['field_imagen_media_product']);
-            this.cardImgR = params.data;
-            if (this.cardImgR.length !== 0) {
-                this.visibleR = true;
-            }
+
+        this._http.getCustomCardInformation('allproductsandservicescard_2').subscribe(items => {
+            this.principalR = this._http.getFilterPrincipalType(items, 'field_right', this.type);
+
+            this._http.getCustomCardInformation('mediaimedicalcard').subscribe(params => {
+                params = this._http.getFilterLists(this.principalR, params);
+                this.titleR = params.header[0].data.title;
+                params.data = this._http.addImageField(params.data, ['field_imagen_media_product']);
+                this.cardImgR = params.data;
+                if (this.cardImgR.length !== 0) {
+                    this.visibleR = true;
+                    this.datosMenu = {
+                        label: 'IMAG R',
+                        id: 'a4',
+                        url: '/imedical'
+                    };
+                    this.propagar.emit(this.datosMenu);
+                }
+            });
         });
     }
 }
