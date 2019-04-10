4956c191f92938fc648c99a13f8edd57290c574f
unknown <alegna.Martinez@bitsamericas.com>
2019-04-10 13:36:47 -0400

    Agregar consumo del nuevo servicio “allproductsandservicescard_2” en componente app-tecnologies-projectservice. Validaciones.

--- angular/src/app/cards/components/tecnologies-projectservice/tecnologies-projectservice.component.ts
+++ angular/src/app/cards/components/tecnologies-projectservice/tecnologies-projectservice.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { CustomCardService } from 'src/app/services/cards/v1-card.services';
 import { DataMenu } from '../menu-template/menu-template.component';
 import { General } from '../blurb/blurb.component';
-
 @Component({
   selector: 'app-tecnologies-projectservice',
   templateUrl: './tecnologies-projectservice.component.html',
@@ -34,15 +33,12 @@ export class TecnologiesProjectserviceComponent implements OnInit {
       this.principalTecnologies = this.https.getFilterPrincipalType(items, 'field_technologies', this.type);
 
       this.https.getCustomCardInformation('expertsinimedicalcard').subscribe(itemsw => {
+        itemsw = this.https.getFilterLists(this.principalTecnologies, itemsw);
         this.title = itemsw.header[0].data.title;
         this.subtitle = itemsw.header[1].data.sub_title;
         itemsw.data = this.https.addImageField(itemsw.data, ['field_tech_color_image']);
         itemsw.data = this.https.addImageField(itemsw.data, ['field_tech_image']);
-
-        const list_items = itemsw.data;
-
-        this.CarouselControlArray = [];
-        this.CarouselControlArray = this.https.getFilterLists(this.principalTecnologies, itemsw.data);
+        this.CarouselControlArray = itemsw.data;
 
         if (this.title !== '' && this.CarouselControlArray.length !== 0) {
           this.visible = true;
