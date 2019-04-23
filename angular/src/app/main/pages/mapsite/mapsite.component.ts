812addf42a46b0b52848353a5510b2bf6cf6dcad
bitsalegnamartinez <alegna.Martinez@bitsamericas.com>
2019-04-23 09:50:51 -0400

    Servicio y HTML del mapa del sitio.

--- angular/src/app/main/pages/mapsite/mapsite.component.ts
+++ angular/src/app/main/pages/mapsite/mapsite.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavbarService } from '../../../services/layout/navbar.service';
 
 @Component({
   selector: 'app-mapsite',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mapsite.component.scss']
 })
 export class MapsiteComponent implements OnInit {
+  public Info;
 
-  constructor() { }
+  constructor(private http: NavbarService) {
+    this.Info = [];
+  }
 
   ngOnInit() {
+    this.getItems();
+  }
+
+  getItems() {
+    return this.http.getLowFooterInfo('mapa-del-sitio---pagina-bit').subscribe(items => {
+      this.Info = items;
+      console.log('items', items);
+    });
   }
 
 }
