61b9b014fea68c9f0b9fa0ea299b39a7d0331ca3
Javier Duarte <javier.duarte@bitsamericas.com>
2019-06-06 16:06:59 -0500

    soporte idioma navbar menu angular

--- angular/src/app/services/layout/navbar.service.ts
+++ angular/src/app/services/layout/navbar.service.ts
@@ -10,12 +10,18 @@ export class NavbarService {
   constructor(private http: HttpService) {
    // console.log('NavBar Listo');
   }
-
+  const langcode = '';
+  getLangcode(){
+    if (window.sessionStorage.getItem('language') != 'es') {
+      this.langcode = window.sessionStorage.getItem('language') + '/';
+    }
+    return this.langcode;
+  }
   getMenuItems() {
-    return this.http.get('adf_menu/menu?_format=json');
+    return this.http.get(this.getLangcode() + 'adf_menu/menu?_format=json');
   }
   getLowFooterInfo (key) {
-    const info = `adf_menu/${key}?_format=json`;
+    const info = this.getLangcode() + `adf_menu/${key}?_format=json`;
     return this.http.get(info);
   }
 }
