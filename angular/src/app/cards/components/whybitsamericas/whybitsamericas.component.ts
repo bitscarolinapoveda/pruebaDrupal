f84f11e04fd2731128c0ac63a1d3cbe33390a1d6
bitsalegnamartinez <alegna.Martinez@bitsamericas.com>
2019-06-25 12:15:27 -0400

    Eliminar console.

--- angular/src/app/cards/components/whybitsamericas/whybitsamericas.component.ts
+++ angular/src/app/cards/components/whybitsamericas/whybitsamericas.component.ts
@@ -30,7 +30,6 @@ export class WhybitsamericasComponent implements OnInit {
 
   getWhybitsInfo() {
     return this.whyBits.getCustomCardInformation('whybitsamericas').subscribe(items => {
-      console.log("item", items);
       this.whyTitle = items.header[0].data.title;
       this.whySubTitle = items.header[1].data.sub_title;
       items.data = this.whyBits.addImageField(items.data, ['field_imagen']);
